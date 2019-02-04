ef752c5246401a04dfc6ae173b262b152d83f96a
Trevor Overby <mr.blarg@gmail.com>
2019-02-04 15:59:29 -0700

    Started reading in NAV log to get UBX pakcet info.

--- pos_data.py
+++ pos_data.py
@@ -1,6 +1,6 @@
 #Position data in .txt format and output distant and/or x/y as a List.
 import sys
-
+import binascii
 #Pixy position data.
 #cam_data is a text file path. String.
 def camera_pos_data(cam_data):
@@ -9,7 +9,17 @@ def camera_pos_data(cam_data):
 #gps_data, text file, HEX. Argument type: String
 #Returns a list of reletive distance measurements in meters.
 def GPS_pos_data(gps_data):
-    pass
+    _data = []
+    try:
+        with open(gps_data, "rb") as gps_file:
+            for line in gps_file:
+                if line[0:4].hex() == 'b5620130': #check first 5 bytes to aquire UBX packet.
+                    print(True)
+        gps_file.closed
+    except(OSError):
+        print("Error in opening/reading file. " + str(OSError))
+        return None
+    return _data
 #UWB distance data.
 #umb_data, text file, ascii. Argument type: String.
 #Returns a list of reletive distance measurements in meters. Otherwise returns None object    
@@ -22,6 +32,7 @@ def UWB_pos_data(uwb_data):
                 if len(data_list) == 8 and (b'\n' in data_list[7]):
                     _data.append(float(data_list[7].strip(b'\n')))
             print(_data[1])
+        uwb_file.closed
     except(OSError):
         print("Error in opening/reading file. " + str(OSError))
         return None
@@ -32,4 +43,4 @@ def UWB_pos_data(uwb_data):
 #If the files are passed in at compile/execution time.
 if __name__ == "__main__":
     #test
-    UWB_pos_data(sys.argv[1])
+    GPS_pos_data(sys.argv[1])
